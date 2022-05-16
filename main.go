1ae26fb5c7c8a1b61d73efa71fb2ad6bdabb31d3
azul915 <k.aoyagi940516@gmail.com>
2022-05-17 00:34:23 +0900

    update

--- main.go
+++ main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"flag"
 	"jitsuyo-go/sec1"
-	"log"
 )
 
 func main() {
-	// sec1.Practice()
-	// sec1.OptionArgs()
-	// sec1.FunctionalOption()
-	// commandLineArgs()
+
 	sec1.PracticeSlice()
 }
 
-func commandLineArgs() {
-	flag.Parse()
-	log.Println(*sec1.FlagStr)
-	log.Println(*sec1.FlagInt)
-	log.Println(flag.Args())
-}
+// func commandLineArgs() {
+// 	flag.Parse()
+// 	log.Println(*sec1.FlagStr)
+// 	log.Println(*sec1.FlagInt)
+// 	log.Println(flag.Args())
+// }
