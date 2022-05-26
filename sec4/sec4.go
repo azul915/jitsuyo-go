0f40f8b7cfedabe6eb132dc492a40ba48d159a88
azul915 <k.aoyagi940516@gmail.com>
2022-05-27 00:07:49 +0900

    update

--- sec4/sec4.go
+++ sec4/sec4.go
@@ -16,10 +16,17 @@ func (d DesktopWarning) Show(message string) {
 	beeep.Alert(os.Args[0], message, "")
 }
 
+func NewDesktopWarning() Warning {
+	return DesktopWarning{}
+}
+
 func IntefacePrac() {
 	// cannot use &(DesktopWarning literal) (value of type *DesktopWarning) as Warning value in variable declaration: *DesktopWarning does not implement Warning (missing method Show)c
 	// var _ Warning = &DesktopWarning{}
 
-	warn := &DesktopWarning{}
+	warn := DesktopWarning{}
 	warn.Show("Hello World to desktop")
+
+	wn := NewDesktopWarning()
+	wn.Show("Hello World to desktop")
 }
