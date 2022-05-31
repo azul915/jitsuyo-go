b3dce59e908e0844dcdfc790933cc9d6d0b932b5
azul915 <k.aoyagi940516@gmail.com>
2022-06-01 01:25:50 +0900

    update

--- sec5/sec5.go
+++ sec5/sec5.go
@@ -3,6 +3,8 @@ package sec5
 import (
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 func Sec5() {
@@ -10,4 +12,27 @@ func Sec5() {
 	fmt.Println(sww.Error())
 	ef := fmt.Errorf("something went wrong: %s", sww.Error())
 	fmt.Println(ef)
+}
+
+type HTTPError struct {
+	StatusCode int
+	URL        string
+}
+
+// type HTTPErrorをレシーバとするError() stringを実装することでErrorインターフェースを満たす
+func (he *HTTPError) Error() string {
+	return fmt.Sprintf("http status code = %d, url = %s", he.StatusCode, he.URL)
+}
+
+func ReadContents(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, &HTTPError{StatusCode: resp.StatusCode, URL: url}
+	}
+	return io.ReadAll(resp.Body)
 }
