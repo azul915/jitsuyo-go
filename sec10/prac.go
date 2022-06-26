8ae25f0993f1b421ed2b57db1fda24cdaba98dab
azul915 <k.aoyagi940516@gmail.com>
2022-06-26 22:51:01 +0900

    update

--- sec10/prac.go
+++ sec10/prac.go
@@ -3,8 +3,8 @@ package sec10
 import "net/http"
 
 func Prac() {
-	http.HandlerFunc(Hello)
-
+	http.HandleFunc("/hello", http.HandlerFunc(Hello))
+	http.ListenAndServe(":3694", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
