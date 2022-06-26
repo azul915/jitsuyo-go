685e696f8a47a428b9be2bd15968731378f30d81
azul915 <k.aoyagi940516@gmail.com>
2022-06-26 23:08:48 +0900

    update

--- sec10/prac.go
+++ sec10/prac.go
@@ -3,10 +3,17 @@ package sec10
 import "net/http"
 
 func Prac() {
-	http.HandleFunc("/hello", Hello)
+	// http.HandleFunc("/hello", Hello)
+	http.Handle("/hello", HelloStruct{})
 	http.ListenAndServe(":3694", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello"))
+}
+
+type HelloStruct struct{}
+
+func (h HelloStruct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello"))
 }
