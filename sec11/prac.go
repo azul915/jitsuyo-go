dac7eb1406a0cb413be662fcef7464a755de58a4
azul915 <k.aoyagi940516@gmail.com>
2022-07-12 10:04:17 +0900

    update

--- sec11/prac.go
+++ sec11/prac.go
@@ -1,6 +1,9 @@
 package sec11
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,8 +13,27 @@ func Prac() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		log.Println("something went wrong!!")
 	}
+	resp.Body.Close()
+
+	type User struct {
+		Name string
+		Addr string
+	}
+
+	u := User{
+		Name: "O'Reilly Japan",
+		Addr: "東京都新宿区四谷坂町",
+	}
+	payload, err := json.Marshal(u)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := http.Post("http://example.com/", "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		log.Fatal(err)
+	}
+	res.Body.Close()
 }
