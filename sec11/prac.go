a71c4d67810aff9cc933f820decc5245a033470c
azul915 <k.aoyagi940516@gmail.com>
2022-08-14 16:03:05 +0900

    add

--- sec11/prac.go
+++ sec11/prac.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"log"
+	"net"
 	"net/http"
 	"time"
 )
@@ -66,4 +68,113 @@ func Prac() {
 	}
 	rr.Body.Close()
 
+	client := &http.Client{
+		Transport: &customRoundTripper{
+			base: http.DefaultTransport,
+		},
+	}
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://example.com", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client.Do(req)
+
+	client = &http.Client{
+		Transport: &loggingRoundTripper{
+			transport: http.DefaultTransport,
+			logger:    nil,
+		},
+	}
+	req, err = http.NewRequestWithContext(ctx, "GET", "http://example.com", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client.Do(req)
+
+	client = &http.Client{
+		Transport: &basicAuthRoundTripper{
+			username: "username",
+			password: "password",
+			base:     http.DefaultTransport,
+		},
+	}
+	req, _ = http.NewRequestWithContext(ctx, "GET", "http://example.com", nil)
+	client.Do(req)
+}
+
+type customRoundTripper struct {
+	base http.RoundTripper
+}
+
+func (c customRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	resp, err := c.base.RoundTrip(req)
+	return resp, err
+}
+
+type loggingRoundTripper struct {
+	transport http.RoundTripper
+	logger    func(string, ...interface{})
+}
+
+func (t *loggingRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	if t.logger == nil {
+		t.logger = log.Printf
+	}
+
+	start := time.Now()
+	resp, err := t.transport.RoundTrip(req)
+	if resp != nil {
+		t.logger("%s %s %d %s, duration: %d", req.Method, req.URL.String(), resp.StatusCode, http.StatusText(resp.StatusCode), time.Since(start))
+	}
+	return resp, err
+}
+
+type basicAuthRoundTripper struct {
+	base     http.RoundTripper
+	username string
+	password string
+}
+
+func (rt *basicAuthRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.SetBasicAuth(rt.username, rt.password)
+	return rt.base.RoundTrip(req)
+}
+
+type retryableRoundTripper struct {
+	base     http.RoundTripper
+	attempts int
+	waitTime time.Duration
+}
+
+func (rt *retryableRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	var (
+		resp *http.Response
+		err  error
+	)
+	for count := 0; count < rt.attempts; count++ {
+		resp, err = rt.base.RoundTrip(req)
+		if !rt.shouldRetry(resp, err) {
+			return resp, err
+		}
+		select {
+		case <-req.Context().Done():
+			return nil, req.Context().Err()
+		case <-time.After(rt.waitTime):
+		}
+	}
+	return resp, err
+}
+func (rt *retryableRoundTripper) shouldRetry(resp *http.Response, err error) bool {
+	if err != nil {
+		var netErr net.Error
+		if errors.As(err, &netErr) && netErr.Temporary() {
+			return true
+		}
+	}
+	if resp != nil {
+		if resp.StatusCode == 429 || (500 <= resp.StatusCode && resp.StatusCode <= 504) {
+			return true
+		}
+	}
+	return false
 }
