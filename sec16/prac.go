3b9fb921a486f5019c26043a55ac3e9dfc4907e7
azul915 <k.aoyagi940516@gmail.com>
2022-08-16 01:00:13 +0900

    add

--- sec16/prac.go
+++ sec16/prac.go
@@ -8,10 +8,9 @@ import (
 func Prac() {
 	items := []int{1, 2, 3, 4, 5, 6, 7, 8}
 	for _, v := range items {
-		v2 := v
-		go func() {
-			fmt.Printf("v2 = %d, address = %p\n", v2, &v2)
-		}()
+		go func(v int) {
+			fmt.Printf("v = %d\n", v)
+		}(v)
 	}
 	time.Sleep(time.Second)
 }
