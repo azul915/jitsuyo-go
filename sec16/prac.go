e005665ea0815998f5209101c1b6b90ebf65dc75
azul915 <k.aoyagi940516@gmail.com>
2022-08-28 21:30:38 +0900

    add

--- sec16/prac.go
+++ sec16/prac.go
@@ -2,17 +2,18 @@ package sec16
 
 import (
 	"fmt"
-	"time"
+	"sync"
+	"sync/atomic"
 )
 
 func Prac() {
-	items := []int{1, 2, 3, 4, 5, 6, 7, 8}
-	for _, v := range items {
-		go func(v int) {
-			fmt.Printf("v = %d\n", v)
-		}(v)
-	}
-	time.Sleep(time.Second)
+	// items := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	// for _, v := range items {
+	// 	go func(v int) {
+	// 		fmt.Printf("v = %d\n", v)
+	// 	}(v)
+	// }
+	// time.Sleep(time.Second)
 
 	// バッファなしintチャネル作成
 	// ic := make(chan int)
@@ -46,12 +47,70 @@ func Prac() {
 	}
 }
 
+// 受信型
 // func recv(r <-chan string) {
 // 	v := <-r
 // 	r <- "送信"// 送信はNG
 // }
 
+// 送信型
 // func send(s chan<- string) {
 // 	s <-"送信OK"
 // 	v := <-s // 受信ダメ
 // }
+
+type Account struct {
+	balance int
+	lock    sync.RWMutex
+}
+
+func (a *Account) GetBalance() int {
+	a.lock.RLock()
+	defer a.lock.RUnlock()
+	return a.balance
+}
+
+func (a *Account) Transfer(amount int) {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	a.balance += amount
+}
+
+type Account2 struct {
+	balance  int
+	transfer chan int
+}
+
+func NewAccount() *Account2 {
+	transfer := make(chan int)
+	r := &Account2{
+		transfer: transfer,
+	}
+	go func() {
+		for {
+			amount := <-transfer
+			r.balance += amount
+		}
+	}()
+	return r
+}
+
+func (a *Account2) GetBalance() int {
+	return a.balance
+}
+
+func (a *Account2) Transfer(amount int) {
+	a.transfer <- amount
+}
+
+type Account3 struct {
+	balance int64
+}
+
+func (a *Account3) GetBalance() int64 {
+	return a.balance
+}
+
+func (a *Account3) Transfer(amount int64) {
+	atomic.AddInt64(&a.balance, amount)
+}
